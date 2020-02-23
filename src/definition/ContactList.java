7acefcbdeefa2ace7e8dfce089315b4c5ac247a2
kashika <kashika.tripathi_cs18@gla.ac.in>
2020-02-23 14:52:51 +0530

    ContactList implements contactADT and its method

--- src/definition/ContactList.java
+++ src/definition/ContactList.java
@@ -1,4 +1,26 @@
 package definition;
 
-public class ContactList {
+import adt.contactADT;
+
+public class ContactList implements contactADT {
+
+    @Override
+    public void add(Object data) {
+
+    }
+
+    @Override
+    public void remove() {
+
+    }
+
+    @Override
+    public void search(String firstName) {
+
+    }
+
+    @Override
+    public void print() {
+
+    }
 }
