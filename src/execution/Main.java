5f61899a9dae27ea6e4ac2deb20ba0542f06abf0
kashika <kashika.tripathi_cs18@gla.ac.in>
2020-02-23 17:16:44 +0530

    created a menu driver program for user

--- src/execution/Main.java
+++ src/execution/Main.java
@@ -1,6 +1,7 @@
 package execution;
 
 import definition.ContactList;
+import definition.MaintainContact;
 
 import java.util.Scanner;
 
@@ -8,6 +9,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ContactList contact = new ContactList();
+        while (true) {
+            MaintainContact maintainContact = new maintainContact();
 
+            System.out.println("Welcome to DBC's Contact List App\n" +
+                    "Press 1 to add a new contact\n" +
+                    "Press 2 to view all contacts\n" +
+                    "Press 3 to search for a contact\n" +
+                    "Press 4 to delete a contact\n" +
+                    "Press 5 to exit program ");
+        }
     }
 }
