36f94d1245f146c6ff4aa7ce2f5cee060c0f7003
kashika <kashika.tripathi_cs18@gla.ac.in>
2020-02-23 14:11:37 +0530

    variables to store firstName,lastName,multiple contact number and email of a person

--- src/helper/Person.java
+++ src/helper/Person.java
@@ -1,4 +1,10 @@
 package helper;
 
+import java.util.ArrayList;
+
 public class Person {
+    private String firstName;
+    private String lastName;
+    private ArrayList<String> number;
+    private String email;
 }
