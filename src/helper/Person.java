e9446d88d3aa6a4dc5635a8e653a700440aabb0a
kashika <kashika.tripathi_cs18@gla.ac.in>
2020-02-23 14:14:41 +0530

    creating getter and setter for firstName,lastName,contact number and email

--- src/helper/Person.java
+++ src/helper/Person.java
@@ -5,6 +5,41 @@ import java.util.ArrayList;
 public class Person {
     private String firstName;
     private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public ArrayList<String> getNumber() {
+        return number;
+    }
+
+    public void setNumber(ArrayList<String> number) {
+        this.number = number;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     private ArrayList<String> number;
     private String email;
+
+
 }
