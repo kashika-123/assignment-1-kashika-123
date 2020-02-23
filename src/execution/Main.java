506c615da808850973408dcdda503d7dbdcd0cf1
kashika <kashika.tripathi_cs18@gla.ac.in>
2020-02-23 17:15:02 +0530

    created an object of scanner class to take user input and an object of ContactList type named contact

--- src/execution/Main.java
+++ src/execution/Main.java
@@ -1,4 +1,13 @@
 package execution;
 
+import definition.ContactList;
+
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ContactList contact = new ContactList();
+
+    }
 }
