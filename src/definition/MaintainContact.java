ded7d99f4aee82842c252267b21dc29db60c87cd
kashika <kashika.tripathi_cs18@gla.ac.in>
2020-02-23 17:05:52 +0530

    first name and last name taken successfully

--- src/definition/MaintainContact.java
+++ src/definition/MaintainContact.java
@@ -9,6 +9,14 @@ public class MaintainContact {
     Scanner sc = new Scanner(System.in);
 
     public Person add() {
+        System.out.println("You have chosen to add a new contact:");
+        System.out.println("Please enter the name of the person");
+        System.out.print("First Name:");
+        person.setFirstName(sc.nextLine());
+        System.out.print("Last Name:");
+        person.setLastName(sc.nextLine());
+
         return person;
+
     }
 }
