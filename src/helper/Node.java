7ca867b18be11510a3bc997e80f1819bcbc480d8
kashika <kashika.tripathi_cs18@gla.ac.in>
2020-02-23 14:26:21 +0530

    creating getter and setter for node type next and for person type data

--- src/helper/Node.java
+++ src/helper/Node.java
@@ -4,4 +4,19 @@ public class Node<Person> {
     private Person data;
     private Node<Person> next;
 
+    public Person getData() {
+        return data;
+    }
+
+    public void setData(Person data) {
+        this.data = data;
+    }
+
+    public Node<Person> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<Person> next) {
+        this.next = next;
+    }
 }
